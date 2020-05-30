78fba7d934de28245a2c69aae1b36e68f8183f09
Marshall Brain <marshalldbrain@gmail.com>
2020-05-30 17:38:02 -0400

    Sign flipping functions for numbers

---
+++ src/commonMain/kotlin/com/marshalldbrain/ion/primitives/Numbers.kt
@@ -0,0 +1,17 @@
+package com.marshalldbrain.ion.primitives
+
+fun Int.flipSign(): Int {
+    return this * -1
+}
+
+fun Long.flipSign(): Long {
+    return this * -1
+}
+
+fun Float.flipSign(): Float {
+    return this * -1
+}
+
+fun Double.flipSign(): Double {
+    return this * -1
+}
